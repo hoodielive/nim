7773e7de25cbcb959a34916c46095d9410a7f658
hoodielive <hoodielive@gmail.com>
2020-12-18 19:30:44 -0500

    Exploring the syntax of nim.

--- Tutorial01/first.nim
+++ Tutorial01/first.nim
@@ -2,6 +2,15 @@
 
 echo "What's your name? "
 
-var name: string = readLine(stdin)
+var name: string = readLine(stdin) 
 
 echo "hi, ", name, "!"
+
+#[
+    Hello, I am a multi
+    line comment and I like
+    peppermint tea. :) 
+]#
+
+let input = readLine(stdin)
+echo input
