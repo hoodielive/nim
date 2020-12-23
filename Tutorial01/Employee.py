294e728535a3b3a05a74d1823eddbc67c6b8dd3d
hoodielive <hoodielive@gmail.com>
2020-12-23 10:09:34 -0500

    Develop with nim and python.

---
+++ Tutorial01/Employee.py
@@ -0,0 +1,14 @@
+class Employee:
+    def __init__(self, career):
+        self._career = career
+
+    def LinuxEngineer(self):
+        if self._career == 'Linux Engineer':
+            return 120_000
+    
+    def PythonDeveloper(self):
+        if self._career == 'Python Developer':
+            return 150_000
+
+linux_admin = Employee("Linux Engineer")
+print("The salary for a Linux Engineer is: ", linux_admin.LinuxEngineer())
