be95fe8bbff917daddc03736b1f3e473a5f3f5eb
hoodielive <hoodielive@gmail.com>
2021-04-20 15:00:40 -0400

    Stuff here and there

---
+++ Tutorial01/variables.nim
@@ -0,0 +1,11 @@
+var variableName = "Value"
+echo variableName, " ", variableName
+
+variableName = "Some other String value"
+
+echo variableName
+
+let immutableVariable = "Unchangeable Value"
+echo immutableVariable
+
+
