5a1ccc7d81c15a998eca621d7506de47369ed800
hoodielive <hoodielive@gmail.com>
2021-04-20 02:33:53 -0400

    simple calculator.

---
+++ Tutorial01/simpleCal.nim
@@ -0,0 +1,47 @@
+import strutils
+
+var
+  input: string
+  num1: float
+  num2: float
+  result: float
+
+while true:
+  echo "Choose your action: Add, Subtract, Divide, Multiply or exit"
+  input = stdin.readLine
+
+  case input:
+    of "Add":
+      stdout.write "First Number: "
+      num1 = stdin.readLine.parseFloat
+      stdout.write "Second Number: "
+      num2 = stdin.readLine.parseFloat
+      result = num1 + num2
+      echo result
+
+    of "Subtract":
+      stdout.write "First Number: "
+      num1 = stdin.readLine.parseFloat
+      stdout.write "Second Number: "
+      num2 = stdin.readLine.parseFloat
+      result = num1 - num2
+      echo result
+
+    of "Multiply":
+      stdout.write "First Number: "
+      num1 = stdin.readLine.parseFloat
+      stdout.write "Second Number: "
+      num2 = stdin.readLine.parseFloat
+      result = num1 * num2
+      echo result
+
+    of "Divide":
+      stdout.write "First Number: "
+      num1 = stdin.readLine.parseFloat
+      stdout.write "Second Number: "
+      num2 = stdin.readLine.parseFloat
+      result = num1 + num2
+      echo result
+
+    of "exit":
+      break
