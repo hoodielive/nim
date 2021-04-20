bc75f552e8dc94eb99b9bc1230f6410e979f52ba
hoodielive <hoodielive@gmail.com>
2021-04-20 01:52:37 -0400

    What in the world is wrong with nimlsp :(

--- Tutorial01/condstate.nim
+++ Tutorial01/condstate.nim
@@ -1,2 +1,12 @@
 if 10 > 5:
   echo "10 is larger than 5"
+
+if "hello" == "Hello":
+  echo "hello is equal to Hello.."
+  if 5 < 50:
+    echo "5 < 50"
+  else:
+    echo "Else #2"
+else:
+  echo "Else #1"
+
