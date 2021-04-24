1720d1d887c6ac66cff4d2b71af549f31622789b
hoodielive <hoodielive@gmail.com>
2021-04-24 02:40:47 -0400

    Jestering

---
+++ jestering/nimweb/main.nim
@@ -0,0 +1,14 @@
+# Import Required Libraries
+
+import db_sqlite              # SQLite.
+import jester                 # Our Webserver.
+import logging                # Logging Utils.
+import os                     # Used to get arguments.
+import parsecfg               # Parse our config file.
+import strutils               # Basic Functions.
+import times                  # Time and Date.
+import uri                    # We need to encode urls: encodeURL()
+import code/database_utils    # Utils used in the database.
+import code/password_utils    # SQLite
+
+
