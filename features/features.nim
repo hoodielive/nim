e403541406694dfc5b8be21bfac731d4c73b9239
hoodielive <hoodielive@gmail.com>
2021-04-25 16:11:40 -0400

    Wow..

--- features/features.nim
+++ features/features.nim
@@ -2,7 +2,7 @@ import strutils
 
 # Nim has style insensitivity.
 # This proc will work because Nim considers identifiers 
-# to be equal.
+# to be equal, because it ignores the underscore.
 
 echo "hello".to_upper()
 echo "hello".toUpper()
