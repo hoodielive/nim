a123ca14bd89fc0336eec90414c5c1944815f2fd
hoodielive <hoodielive@gmail.com>
2021-04-29 21:33:21 -0400

    Okay, parsing json skills are weak asf

--- nimbo-chat/nimbo_chat/src/protocol.nim
+++ nimbo-chat/nimbo_chat/src/protocol.nim
@@ -5,8 +5,29 @@ type
     username*: string
     message*: string
 
+  MessageParsingError* = object of Exception
+
 proc parseMessage*(data: string): Message =
   let dataJson = parseJson(data)
+  result.username = dataJson["username"].getStr()
+  result.message = dataJson["username"].getStr()
+
+when isMainModule:
+  block: 
+    let data = """{ "username": "Esu", "message": "Alaffia!" }"""
+    let parsed = parseMessage(data)
+    doAssert parsed.username == "Esu"
+    doAssert parsed.message == "Alaffia!"
+
+  block:
+    try:
+      let parsed = parseMessage("data")
+    except MessageParsingError:
+      doAssert true
+    except: 
+      doAssert false
+
+    echo "All test(s) have passed."
 
 assert parseJson("null").kind == JNull
 assert parseJson("true").kind == JBool
