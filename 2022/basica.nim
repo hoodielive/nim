5be265fe10f09cb10aa4d5bec6fb8b368a98b073
hoodielive <hoodielive@gmail.com>
2022-01-14 01:51:38 -0500

    let const

--- 2022/basica.nim
+++ 2022/basica.nim
@@ -7,7 +7,7 @@ echo number
 
 echo "The whole number is", number
 
-var name_again: string = "Lawrence"
+var nameAgain: string = "Lawrence"
 
 # Nim does have type inference.
 
@@ -18,3 +18,14 @@ var b = 7 # is permitted.
 
 echo b, a
 
+echo nameAgain
+
+var # runs at run time.
+  f = -3
+  g = 3
+  e = 99
+  h: string = "error in this"
+
+echo "f\n"
+
+const BUFFER: int = 4 # runs at compile time because it's const
