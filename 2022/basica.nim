dc728f6f7b20dec4823d4b916873ff6dfc994711
hoodielive <hoodielive@gmail.com>
2022-01-14 01:37:30 -0500

    Refresh.

--- 2022/basica.nim
+++ 2022/basica.nim
@@ -4,5 +4,17 @@ let decimal = 1.0
 let booleanvar = false 
 
 echo number
+
 echo "The whole number is", number
+
+var name_again: string = "Lawrence"
+
+# Nim does have type inference.
+
+var a: int = 1
+
+# However.
+var b = 7 # is permitted.
+
+echo b, a
 
