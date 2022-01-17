fca28fbbc190d36eb9afa4198f51729763f8a071
hoodielive <hoodielive@gmail.com>
2022-01-17 00:03:09 -0500

    Damn.

---
+++ 2022/relationalOperators.nim
@@ -0,0 +1,19 @@
+let
+  i: char = 'a'
+  j: char = 'd'
+  k: char = 'Z'
+
+echo i < j
+echo i < k
+
+let
+  m: string = "axyb"
+  n: string = "axyz"
+  o: string = "ba"
+  p: string = "ba "
+
+echo m < n
+echo n < o
+echo o < p
+
+
